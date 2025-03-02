01ee32372c38317e61ff66040b59adb661c77d1a
DaliborZ-tech <dalibor.zalesak@gmail.com>
2025-03-02 16:32:24 +0100

    Accounts - authentication - fixed navbar

--- djangoProjectHollymoviesSDA/settings.py
+++ djangoProjectHollymoviesSDA/settings.py
@@ -136,3 +136,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = 'home'
 LOGIN_URL = 'login'
 LOGOUT_URL = 'logout'
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
